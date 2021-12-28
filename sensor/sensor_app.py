b125973a846df276a07989d38e5a40383ae8fd6b
apoorv mathur <apoorv.mathur@insead.edu>
2021-12-27 23:45:06 -0800

    update

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,13 +1,10 @@
 # Runner script for all modules
 from load_data import load_sensor_data
-
 ##############################
 # Do not remove these two lines
 # They are needed to validate your unittest
 data = []
 print("Sensor Data App")
-
-
 ##############################
 
 # Module 1 code here:
